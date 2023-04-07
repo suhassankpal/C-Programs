ee5dcf2e5e6a96bf6dcf3f033cc2b0b518a63bf8
suhassankpal <119999060+suhassankpal@users.noreply.github.com>
2023-04-07 13:27:11 +0530

    Accept number from user and return largest digit from that number. (optimization by Using break keyword)

---
+++ program65.c
@@ -0,0 +1,51 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Problem statement:Accept number from user and return largest digit from that number
+// INPUT:721                  |INPUT:958               |OUTPUT:524
+// OUTPUT:7                   |OUTPUT:9                |INPUT:5
+//(optimization by Using break keyword)
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+int MaxDigit(int iNo)
+{
+    int iDigit = 0;
+    int iMax = 0;
+
+    if (iNo < 0)
+    {
+        iNo = -(iNo);
+    }
+
+    while (iNo != 0)
+    {
+        iDigit = iNo % 10;
+
+        if (iDigit > iMax)
+        {
+            iMax = iDigit;
+        }
+
+        if (iMax == 9)
+        {
+            break;
+        }
+
+        iNo = iNo / 10;
+    }
+    return iMax;
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter number\n");
+    scanf("%d", &iValue);
+
+    iRet = MaxDigit(iValue);
+    printf("Greater digit from %d is : %d", iValue, iRet);
+
+    return 0;
+}
