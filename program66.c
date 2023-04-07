5cd5146d6158b14170ad0b34ee60d6987950252d
suhassankpal <119999060+suhassankpal@users.noreply.github.com>
2023-04-07 13:27:32 +0530

    ccept number from user and return Smallest digit from that number

---
+++ program66.c
@@ -0,0 +1,45 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Problem statement:Accept number from user and return Smallest digit from that number
+// INPUT:721                  |INPUT:958               |OUTPUT:524
+// OUTPUT:1                   |OUTPUT:5                |INPUT:2
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+int MinDigit(int iNo)
+{
+    int iDigit = 0;
+    int iMin = 9;
+
+    if (iNo < 0)
+    {
+        iNo = -(iNo);
+    }
+
+    while (iNo != 0)
+    {
+        iDigit = iNo % 10;
+
+        if (iDigit < iMin)
+        {
+            iMin = iDigit;
+        }
+
+        iNo = iNo / 10;
+    }
+    return iMin;
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter number\n");
+    scanf("%d", &iValue);
+
+    iRet = MinDigit(iValue);
+    printf("Smallest digit from %d is : %d", iValue, iRet);
+
+    return 0;
+}
