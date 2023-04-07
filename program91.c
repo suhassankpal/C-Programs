f74f791243f2ba6045f4003e7f6658af482ddab8
suhassankpal <119999060+suhassankpal@users.noreply.github.com>
2023-04-07 13:47:47 +0530

    Write a program which accept number from user and display all numbers till that number in reverse manner

---
+++ program91.c
@@ -0,0 +1,36 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//**************************************PATTERN PRINTING*************************************************************
+//
+// Problem statement:
+// Write a program which accept number from user and display all numbers till that number in reverse manner
+//
+// INPUT: 4
+// OUTPUT :4  3  2  1
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#include <stdio.h>
+
+void Display(int iValue)
+{
+    int iCnt = 0;
+
+    if (iValue <= 0)
+    {
+        iValue = -(iValue);
+    }
+
+    for (iCnt = iValue; iCnt >= 1; iCnt--)
+    {
+        printf("%d\t", iCnt);
+    }
+}
+
+int main()
+{
+    int iNo = 0;
+    printf("Enter number:\n");
+    scanf("%d", &iNo);
+
+    Display(iNo);
+    return 0;
+}
