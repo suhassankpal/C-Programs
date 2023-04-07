6d29cfe764523e95ef4c8ec906e9917fc0df74f7
suhassankpal <119999060+suhassankpal@users.noreply.github.com>
2023-04-07 12:55:09 +0530

    Accept number from user and check whether it is divisible by 5 and 3

---
+++ program11.c
@@ -0,0 +1,39 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Problem statement- Accept number from user and check whether it is divisible by 5 and 3
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+#include <stdbool.h>
+
+bool DivisibleByThreeAndFive(int iValue)
+{
+    if ((iValue % 3 == 0) && (iValue % 5 == 0))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int main()
+{
+    int iNo = 0;
+    bool bRet = false;
+
+    printf("Enter number :\n");
+    scanf("%d", &iNo);
+
+    bRet = DivisibleByThreeAndFive(iNo);
+
+    if (bRet == true)
+    {
+        printf("%d is divisible by 3 and 5", iNo);
+    }
+    else
+    {
+        printf("%d is not divisible by 3 and 5", iNo);
+    }
+    return 0;
+}
