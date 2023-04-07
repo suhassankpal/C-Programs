ca44e77d22b38c116dc4a6510249bed184229918
suhassankpal <119999060+suhassankpal@users.noreply.github.com>
2023-04-07 13:20:01 +0530

    Accept number from user and return sumation of digits

---
+++ program52.c
@@ -0,0 +1,37 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Problem statement:Accept number from user and return sumation of digits
+// INPUT:751          |INPUT:7515        |OUTPUT:75
+// OUTPUT: 13          |OUTPUT:18          |INPUT:12
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+int SumDigits(int iValue)
+{
+    int iDigit = 0;
+    int iSum = 0;
+
+    while (iValue != 0)
+    {
+        iDigit = iValue % 10;
+        iValue = iValue / 10;
+
+        iSum = iSum + iDigit;
+    }
+    return iSum;
+}
+
+int main()
+{
+    int iNo = 0;
+    int iRet = 0;
+
+    printf("Enter number:\n");
+    scanf("%d", &iNo);
+
+    iRet = SumDigits(iNo);
+
+    printf("Sumation of digits is : %d", iRet);
+
+    return 0;
+}
