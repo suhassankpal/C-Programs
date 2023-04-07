51bbc6473cfcfae5065bac1170b7020ae5607bfb
suhassankpal <119999060+suhassankpal@users.noreply.github.com>
2023-04-07 13:06:08 +0530

    pritnt Marvellous : 1 to Marvellous : 5 Demonstration of sequence (simple way but this is worst case)

---
+++ program21.c
@@ -0,0 +1,22 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Problem statement- pritnt Marvellous : 1 to Marvellous : 5
+//
+// Demonstration of sequence (simple way but this is worst case)
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+void Display()
+{
+    printf("Marvellous : 1\n");
+    printf("Marvellous : 2\n");
+    printf("Marvellous : 3\n");
+    printf("Marvellous : 4\n");
+    printf("Marvellous : 5\n");
+}
+int main()
+{
+    Display();
+
+    return 0;
+}
