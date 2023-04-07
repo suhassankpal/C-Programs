b595c0a62a329134bb4a70692546139d03787e90
suhassankpal <119999060+suhassankpal@users.noreply.github.com>
2023-04-07 13:25:01 +0530

    Accept number from user and check whether number is palindrome or not  (using three function)

---
+++ program61.c
@@ -0,0 +1,53 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Problem statement:Accept number from user and check whether number is palindrome or not 
+// INPUT:121                  |INPUT:707               |OUTPUT:111
+// OUTPUT:121                 |OUTPUT:707              |INPUT:111
+//(using three function)
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+#include <stdbool.h>
+
+int Reverse(int iNo)
+{
+    int iDigit = 0, iRev = 0;
+
+    while (iNo != 0)
+    {
+        iDigit = iNo % 10;
+        iRev = (iRev * 10) + iDigit;
+
+        iNo = iNo / 10;
+    }
+    return iRev;
+}
+
+int CheckPalindrome(int iData)
+{
+    int iReverse = 0;
+    iReverse = Reverse(iData);
+
+    return (iReverse == iData);
+}
+
+int main()
+{
+    int iValue = 0;
+    bool bRet = false;
+
+    printf("Enter number\n");
+    scanf("%d", &iValue);
+
+    bRet = CheckPalindrome(iValue);
+
+    if (bRet == true)
+    {
+        printf("%d is palindrome", iValue);
+    }
+    else
+    {
+        printf("%d is not palindrome", iValue);
+    }
+
+    return 0;
+}
