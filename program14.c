0812d2752da4241e38c8eaaddf36ddb59a06efbc
suhassankpal <119999060+suhassankpal@users.noreply.github.com>
2023-04-07 12:56:49 +0530

    pritnt statement multiple times (Demonstration of sequence  (without functional Approach))

---
+++ program14.c
@@ -0,0 +1,18 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Problem statement- pritnt statement multiple times          (Ex.print statement 5 times below is simple way)
+//
+// Demonstration of sequence  (without functional Approach) completly sequence
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+int main()
+{
+    printf("JAY GANESH.....\n");
+    printf("JAY GANESH.....\n");
+    printf("JAY GANESH.....\n");
+    printf("JAY GANESH.....\n");
+    printf("JAY GANESH.....\n");
+
+    return 0;
+}
