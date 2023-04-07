fef9e87a524ca0d4f14aba5270fa97824c87415a
suhassankpal <119999060+suhassankpal@users.noreply.github.com>
2023-04-07 13:51:06 +0530

    Write a program which accept number from user and display character in below format

---
+++ program98.c
@@ -0,0 +1,39 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//**************************************PATTERN PRINTING*************************************************************
+//
+// Problem statement:
+// Write a program which accept number from user and display character in below format
+//
+// INPUT: 4
+// OUTPUT :a   b   c   d
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#include <stdio.h>
+// ASCII
+// American Standard Code for Information Interchange
+
+void Display(int iValue)
+{
+    int iCnt = 0;
+    char ch = 'a';
+
+    if (iValue <= 0)
+    {
+        iValue = -(iValue);
+    }
+
+    for (iCnt = 1; iCnt <= iValue; iCnt++, ch++)
+    {
+        printf("%c\t", ch);
+    }
+}
+
+int main()
+{
+    int iNo = 0;
+    printf("Enter number:\t");
+    scanf("%d", &iNo);
+
+    Display(iNo);
+    return 0;
+}
